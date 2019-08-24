ecedf9f24d8b1429f85a0f9328861ac8bd9ed9ff
Ludvig Sundström <lud.sund@gmail.com>
2019-08-24 16:59:41 +0200

    Implement interaction and report generator

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,14 +2,38 @@
 
 module Lib where
 
+import Control.Monad
+import Data.Function
+import Data.List (find, groupBy, sortBy)
 import Data.List.Split (splitOn)
+import Data.Maybe
+
+data Category
+  = GroceryFarmacy
+  | EverydayLife
+  | Restaurant
+  | Nightlife
+  | FastFood
+  | Technology
+  | Apartment
+  | SharedAccount
+  | Vacation
+  | Auto
+  | PublicTransport
+  | Subscriptions
+  | Cash
+  | Sport
+  | Clothes
+  | Hairdresser
+  | Unknown
+  deriving (Show, Eq, Ord)
 
 csvFile :: FilePath
 csvFile =
   "/home/lsund/Documents/personal/economy/helena/bank-summary/apr/20190818-123093569-umsatz.CSV"
 
-data TransactionInfo =
-  TransactionInfo
+data Transaction =
+  Transaction
     { _type :: String
     , _ocr :: String
     , _amount :: Maybe Double
@@ -34,18 +58,73 @@ readMaybe s =
 readAmount :: [String] -> Maybe Double
 readAmount xs = (readMaybe . map commaToPeriod . peel . (!!) xs) 14
 
-readToken :: [String] -> Int -> String
-readToken ss = peel . (!!) ss
-
-fromString :: String -> TransactionInfo
+fromString :: String -> Transaction
 fromString s =
-  TransactionInfo
+  Transaction
     (readToken split 3)
     (readToken split 4)
     (readAmount split)
     (readToken split 11)
   where
+    readToken ss = peel . (!!) ss
     split = splitOn ";" s
 
-someFunc :: IO ()
-someFunc = readFile csvFile >>= mapM_ (print . fromString) . lines
+transactions :: String -> [Transaction]
+transactions = map fromString . lines
+
+balance :: [Transaction] -> Maybe Double
+balance = msum . map _amount
+
+categoryMapping :: [(Char, Category)]
+categoryMapping =
+  [ ('m', GroceryFarmacy)
+  , ('e', EverydayLife)
+  , ('r', Restaurant)
+  , ('n', Nightlife)
+  , ('f', FastFood)
+  , ('t', Technology)
+  , ('a', Apartment)
+  , ('v', Vacation)
+  , ('o', Auto)
+  , ('s', Subscriptions)
+  , ('x', Sport)
+  , ('c', Cash)
+  , ('k', Clothes)
+  , ('h', Hairdresser)
+  , ('u', Unknown)
+  ]
+
+userQuery :: Transaction -> IO (Category, Maybe Double)
+userQuery t = do
+  mapM_ print categoryMapping
+  print t
+  (x:_) <- getLine
+  let cat = fromMaybe Unknown $ snd <$> find ((== x) . fst) categoryMapping
+  return (cat, _amount t)
+
+interactiveGroup :: IO [(Category, Maybe Double)]
+interactiveGroup = readFile csvFile >>= mapM userQuery . tail . transactions
+
+dummy :: [(Category, Maybe Double)]
+dummy =
+  [ (Nightlife, Just 10)
+  , (FastFood, Just 5)
+  , (Nightlife, Just 3)
+  , (Unknown, Nothing)
+  ]
+
+categorySums :: [(Category, Maybe Double)] -> [(Category, Double)]
+categorySums xs =
+  zip (map (fst . head) xss) (map (sum . catMaybes . map snd) xss)
+  where
+    xss = groupCategory xs
+
+categorySumToString :: [(Category, Double)] -> String
+categorySumToString =
+  foldr (\(x, y) acc -> show x ++ "," ++ show y ++ "\n" ++ acc) ""
+
+groupCategory :: [(Category, Maybe Double)] -> [[(Category, Maybe Double)]]
+groupCategory = groupBy (on (==) fst) . sortBy (on compare fst)
+
+genReport :: [(Category, Maybe Double)] -> IO ()
+genReport = writeFile "test.txt" . categorySumToString . categorySums
