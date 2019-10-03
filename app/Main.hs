3b3529a89e7facb560a3573d5f9dad9dc8b6ae47
Ludvig Sundström <lud.sund@gmail.com>
2019-10-03 06:14:26 +0200

    Add read file or input function

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,15 @@ module Main where
 
 import Control.Monad.Extra (allM)
 import Data.Aeson
-import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString.Lazy as BL
+import qualified Codec.Binary.UTF8.String as UTF8
+-- import Data.ByteString.UTF8 (toString)
 import Filter
 import Options.Applicative
-import Report
 import System.Directory (doesFileExist)
 import Transaction
+import Data.Text (Text)
+import Data.Text as T
 
 readTransactions :: FilePath -> IO [Transaction]
 readTransactions = fmap transactionsFromString . readFile
@@ -22,18 +25,25 @@ data OptSpec =
 parseOptSpec :: Parser OptSpec
 parseOptSpec =
   OptSpec <$>
-  strOption
-    (long "input" <> short 'i' <> metavar "FILE" <>
-     help "Input file (exported sparkasse csv)") <*>
-  strOption
-    (long "filter-spec" <> short 'f' <> metavar "FILE" <>
-     help "File containing filter specificatation") <*>
+  argument str
+    (metavar "INPUT" <>
+     help "Sparkasse Input (CSV))") <*>
+  argument str
+    (metavar "FILTERS" <>
+     help "Filter Spec (JSON)") <*>
   optional
     (strOption
        (long "output" <> short 'o' <> metavar "FILE" <>
         help
           ("Output file (exported sparkasse csv). " ++
            "If omitted, print to stdout")))
+
+readFileOrInput :: String -> IO BL.ByteString
+readFileOrInput input = do
+  x <- doesFileExist input
+  if x
+    then BL.readFile input
+    else return $ BL.pack $ UTF8.encode input
 
 run :: OptSpec -> IO ()
 run (OptSpec input filterSpec output) = do
@@ -42,7 +52,7 @@ run (OptSpec input filterSpec output) = do
   if inputFilesExist
     then do
       ts <- readTransactions input
-      jsonStr <- BS.readFile filterSpec
+      jsonStr <- BL.readFile filterSpec
       let mfs = decode jsonStr :: Maybe [Filter]
       case mfs of
         Just fs -> do
@@ -50,9 +60,9 @@ run (OptSpec input filterSpec output) = do
                 (encode . assignedAndUnmatched ts) fs
           case output of
             Just outputFile -> do
-              BS.writeFile outputFile reportJSON
+              BL.writeFile outputFile reportJSON
               putStrLn $ "Success: wrote '" ++ outputFile ++ "'"
-            Nothing -> BS.putStr reportJSON
+            Nothing -> BL.putStr reportJSON
         Nothing -> putStrLn $ "Error: Could not parse '" ++ filterSpec ++ "'"
     else putStrLn $ "A file in " ++ show requiredFiles ++ " was not found."
 
